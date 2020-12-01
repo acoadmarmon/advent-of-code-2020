e5836e4368e9a91430f72fdc1f54aad3b62ac81c
Andrew Marmon <amarmon3@gatech.edu>
2020-12-01 11:08:01 -0500

    rank 307 before reset

--- day-1/day1.py
+++ day-1/day1.py
@@ -3,23 +3,23 @@ import numpy as np
 f = open('input.txt', 'r')
 input_text = [val.split('\n')[0] for val in f.readlines()]
 
-print(input_text)
-
 input_array = np.asarray([int(i) for i in input_text])
 
-# Part 1
-for idx, i in enumerate(input_array):
-    for jdx, j in enumerate(input_array):
-        if idx != jdx:
-            if i + j == 2020:
-                print(input_array[idx]*input_array[jdx])
-                #exit(0)
+def part_1():
+    for idx, i in enumerate(input_array):
+        for jdx, j in enumerate(input_array):
+            if idx != jdx:
+                if i + j == 2020:
+                    return i*j
 
-# Part 2
-for idx, i in enumerate(input_array):
-    for jdx, j in enumerate(input_array):
-        for kdx, k in enumerate(input_array):
-            if idx != jdx and idx != kdx:
-                if i + j + k == 2020:
-                    print(input_array[idx]*input_array[jdx]*input_array[kdx])
-                    exit(0)
+def part_2():
+    for idx, i in enumerate(input_array):
+        for jdx, j in enumerate(input_array):
+            for kdx, k in enumerate(input_array):
+                if idx != jdx and idx != kdx:
+                    if i + j + k == 2020:
+                        return i*j*k
+
+
+print('Part 1 answer is {}'.format(part_1()))
+print('Part 2 answer is {}'.format(part_2()))
